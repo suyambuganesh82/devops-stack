9ec0c4b62f17c1d0f56872a17431b47b21ca635e
Léo Depriester <leo.depriester@camptocamp.com>
2022-02-18 16:59:31 +0100

    feat: add support of scaleway

---
+++ modules/scaleway/outputs.tf
@@ -0,0 +1,13 @@
+#output "base_domain" {
+#  value = local.base_domain
+#}
+#
+#output "lb_ip_address" {
+#  value = scaleway_lb_ip.this.ip_address
+#}
+#
+#output "kubernetes" {
+#  value     = local.kubernetes
+#  sensitive = true
+#}
+#
